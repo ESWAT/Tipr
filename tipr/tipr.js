a5a67502ed28734cc153011437c69a272b738287
Philip Karpiak <operations@eswat.ca>
2014-03-28 16:31:48 -0400

    Syntax improvements

--- tipr/tipr.js
+++ tipr/tipr.js
@@ -9,63 +9,60 @@ http://www.tipue.com/tipr
 
 (function($) {
 
-     $.fn.tipr = function(options) {
+    $.fn.tipr = function(options) {
 
-          var set = $.extend( {
+        var deviceHasTouch = !!('ontouchstart' in window),
+            deviceHasMouse = !!('onmousemove' in window),
+            set = $.extend({
+                'speed' : 200,
+                'mode'  : 'bottom'
+            }, options);
 
-               'speed'        : 200,
-               'mode'         : 'bottom'
+        if (!deviceHasTouch && deviceHasMouse) {
 
-          }, options);
+            return this.each(function() {
 
-          var deviceHasTouch = !!('ontouchstart' in window);
-          var deviceHasMouse = !!('onmousemove' in window);
+                var tipr_cont  = '[class^=tipr_container_]',
+                    tipr_point = '[class^=tipr_point_]';
 
-          if (!deviceHasTouch && deviceHasMouse) {
+                $(this).hover(
+                    function() {
+                        var out = '<div class="tipr_container_' + set.mode + '">';
+                            out += '<div class="tipr_point_' + set.mode + '">';
+                            out += '<div class="tipr_content">';
+                            out += $(this).attr('data-tip') + '</div></div></div>';
+                        $(this).append(out);
 
-               return this.each(function() {
+                        var w_t = $(tipr_cont).outerWidth(),
+                            w_e = $(this).width(),
+                            h_e = $(this).outerHeight(),
+                            h_t = $(tipr_cont).outerHeight(),
+                            m_l = (w_e / 2) - (w_t / 2),
+                            v_h = $(window).height() + $(window).scrollTop(),
+                            hitbox = $(this).offset().top + h_e + h_t,
+                            show_top = false;
 
-                    var tipr_cont = '[class^=tipr_container_]';
-                    var tipr_point = '[class^=tipr_point_]';
+                        $(tipr_cont).css('margin-left', m_l + 'px');
+                        $(this).removeAttr('title');
 
-                    $(this).hover(
-                         function ()
-                         {
-                              var out = '<div class="tipr_container_' + set.mode + '"><div class="tipr_point_' + set.mode + '"><div class="tipr_content">' + $(this).attr('data-tip') + '</div></div></div>';
-                              $(this).append(out);
+                        if (hitbox > v_h && set.mode === 'bottom') {
+                            show_top = true;
+                            $(tipr_cont).removeClass('tipr_container_bottom').addClass('tipr_container_top');
+                            $(tipr_point).removeClass('tipr_point_bottom').addClass('tipr_point_top');
+                        }
 
-                              var w_t = $(tipr_cont).outerWidth();
-                              var w_e = $(this).width();
-                              var h_e = $(this).outerHeight();
-                              var h_t = $(tipr_cont).outerHeight();
-                              var m_l = (w_e / 2) - (w_t / 2);
-                              var v_h = $(window).height() + $(window).scrollTop();
+                        if (set.mode === 'top' || show_top === true) {
+                            $(tipr_cont).css('margin-top', '-' + (h_e + h_t) + 'px');
+                        }
 
-                              var hitbox = $(this).offset().top + h_e + h_t;
-                              var show_top = false;
-
-                              $(tipr_cont).css('margin-left', m_l + 'px');
-                              $(this).removeAttr('title');
-
-                              if (hitbox > v_h && set.mode === 'bottom') {
-                                show_top = true;
-                                $(tipr_cont).removeClass('tipr_container_bottom').addClass('tipr_container_top');
-                                $(tipr_point).removeClass('tipr_point_bottom').addClass('tipr_point_top');
-                              }
-
-                              if (set.mode === 'top' || show_top === true)
-                                $(tipr_cont).css('margin-top', '-' + (h_e + h_t) + 'px');
-
-                              $(tipr_cont).fadeIn(set.speed);
-                         },
-                         function ()
-                         {
-                              $(tipr_cont).remove();
-                         }
-                    );
-
-               });
-          };
-     };
+                        $(tipr_cont).fadeIn(set.speed);
+                    },
+                    function() {
+                        $(tipr_cont).remove();
+                    }
+                );
+            });
+        }
+    };
 
 })(jQuery);
